368ed4929064c443be810c8fcc3e2b3af6ef3781
Alex Antonyuk <alex@antonyuk.me>
2022-04-02 19:42:04 +0300

    ok

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ const octokit = github.getOctokit(core.getInput('token'));
 async function main(name) {
     console.log(`Hello ${name}`);
     core.setOutput('poop', '💩');
-
-    await octokit.rest.pulls.create({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        title: `Hello ${name}`,
-        body: "Lorem shitsum "
-    });
 }
 
 const name = core.getInput("name");
